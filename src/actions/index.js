5127359254dc6124c351a004c4f76945cd1ae4fc
Ross Dyson <dyson.ross@gmail.com>
2017-02-28 23:36:27 +1100

    init commit

---
+++ src/actions/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const SEND_MESSAGE = 'SEND_MESSAGE';
+
+const ENDPOINT_URL = 'http://139.59.109.208:5000'
+
+export function sendMessage(props) {
+  const request = axios({
+    method:'POST',
+    url: `${ENDPOINT_URL}/send/`,
+    data: {
+      message: props.message,
+    }
+  });
+  
+  return {
+    type: SEND_MESSAGE,
+    payload: request
+  }
+}
--- src/containers/home.js
+++ src/containers/home.js
@@ -1,13 +1,69 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Button, FormGroup, FormControl, ControlLabel} from 'react-bootstrap';
 
+import {sendMessage} from '../actions/index';
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      message: ''
+    };
+    
+    this.validateMessage = this.validateMessage.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+  }
+  
+  handleChange(e) {
+    this.setState({message: e.target.value});
+  }
+  
+  validateMessage(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
+  
+  submitMessage(e) {
+    e.preventDefault();
+    sendMessage(this.state);
+  }
+  
   render() {
     return (
       <div>
-        <ul></ul>
-        <p>Sup Ross</p>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 col-md-6 col-md-offset-3">
+              <h1 className="header-text text-center">text oliver a sms message</h1>
+              <form onSubmit={this.submitMessage}>
+                <FormGroup
+                  controlId="sendMessage"
+                >
+                  <ControlLabel>Your message:</ControlLabel>
+                  <FormControl
+                    bsClass="form-control form-control-tall"
+                    componentClass="textarea"
+                    maxLength="70"
+                    value={this.state.message}
+                    placeholder="Enter text"
+                    onChange={this.handleChange}
+                  />
+                  <FormControl.Feedback />
+                </FormGroup>
+                <Button
+                  block
+                  bsStyle="primary"
+                  type="submit"
+                >
+                  Send
+                </Button>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
